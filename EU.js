e23ad42b8760bc84274410764174eb2073036b7b
Rudra Pratap Singh <24bcs156@ietdavv.edu.in>
2025-09-24 15:25:06 +0530

    lala

---
+++ EU.js
@@ -0,0 +1,906 @@
+[
+  {
+    "email": "prashasht61@gmail.com",
+    "uniqueCode": "1"
+  },
+  {
+    "email": "avnipandey0723@gmail.com",
+    "uniqueCode": "2"
+  },
+  {
+    "email": "atharvporwal53@gmail.com",
+    "uniqueCode": "3"
+  },
+  {
+    "email": "anshikabhand09@gmail.com",
+    "uniqueCode": "4"
+  },
+  {
+    "email": "adityagupta86548@gmail.com",
+    "uniqueCode": "5"
+  },
+  {
+    "email": "shivambose07@gmail.com",
+    "uniqueCode": "6"
+  },
+  {
+    "email": "parmarkumkum2007@gmail.com",
+    "uniqueCode": "7"
+  },
+  {
+    "email": "avaniupadhyay17@gmail.com",
+    "uniqueCode": "8"
+  },
+  {
+    "email": "ektabare6@gmail.com",
+    "uniqueCode": "9"
+  },
+  {
+    "email": "bhinijain2006@gmail.com",
+    "uniqueCode": "10"
+  },
+  {
+    "email": "mehulaithekar@gmail.com",
+    "uniqueCode": "11"
+  },
+  {
+    "email": "mananc2007@gmail.com",
+    "uniqueCode": "12"
+  },
+  {
+    "email": "umang.b@gmail.com",
+    "uniqueCode": "14"
+  },
+  {
+    "email": "priyankanihalani9@gmail.com",
+    "uniqueCode": "15"
+  },
+  {
+    "email": "suhanihedau5@gmail.com",
+    "uniqueCode": "16"
+  },
+  {
+    "email": "prakharbandi7@gmail.com",
+    "uniqueCode": "17"
+  },
+  {
+    "email": "vinay1006r@gmail.com",
+    "uniqueCode": "18"
+  },
+  {
+    "email": "kavyanshsaxena23@gmail.com",
+    "uniqueCode": "19"
+  },
+  {
+    "email": "priyalsoni9528@gmail.com",
+    "uniqueCode": "20"
+  },
+  {
+    "email": "ps25.pragya@gmail.com",
+    "uniqueCode": "21"
+  },
+  {
+    "email": "agratachaturvedi8@gmail.com",
+    "uniqueCode": "22"
+  },
+  {
+    "email": "24btc041@ietdavv.edu.in",
+    "uniqueCode": "23"
+  },
+  {
+    "email": "anitadhakad333@gmail.com",
+    "uniqueCode": "24"
+  },
+  {
+    "email": "23bcs101@ietdavv.edu.in",
+    "uniqueCode": "25"
+  },
+  {
+    "email": "parsaivaidik25@gmail.com",
+    "uniqueCode": "26"
+  },
+  {
+    "email": "angelkalra122046@gmail.com",
+    "uniqueCode": "27"
+  },
+  {
+    "email": "parthjagwani9@gmail.com",
+    "uniqueCode": "28"
+  },
+  {
+    "email": "khandelwalmahi004@gmail.com",
+    "uniqueCode": "29"
+  },
+  {
+    "email": "9664047628pawan@gmail.com",
+    "uniqueCode": "30"
+  },
+  {
+    "email": "Vedikagangrade0111@gmail.com",
+    "uniqueCode": "31"
+  },
+  {
+    "email": "suryanshteepa1@gmail.com",
+    "uniqueCode": "32"
+  },
+  {
+    "email": "patelrashik247@gmail.com",
+    "uniqueCode": "33"
+  },
+  {
+    "email": "aryanmasani9@gmail.com",
+    "uniqueCode": "34"
+  },
+  {
+    "email": "naitik9575@gmail.com",
+    "uniqueCode": "35"
+  },
+  {
+    "email": "nehadsoni@gmail.com",
+    "uniqueCode": "36"
+  },
+  {
+    "email": "bankeyharsh11@gmail.com",
+    "uniqueCode": "37"
+  },
+  {
+    "email": "utkarshpandey3456@gmail.com",
+    "uniqueCode": "38"
+  },
+  {
+    "email": "jaydevverma029@gmail.com",
+    "uniqueCode": "39"
+  },
+  {
+    "email": "tajhatim8@gmail.com",
+    "uniqueCode": "40"
+  },
+  {
+    "email": "24bcs034@ietdavv.edu.in",
+    "uniqueCode": "41"
+  },
+  {
+    "email": "24bcb073@ietdavv.edu.in",
+    "uniqueCode": "42"
+  },
+  {
+    "email": "aishwaryamandlecha13@gmail.com",
+    "uniqueCode": "43"
+  },
+  {
+    "email": "shivamgupta262007@gmail.com",
+    "uniqueCode": "44"
+  },
+  {
+    "email": "2006jainparv@gmail.com",
+    "uniqueCode": "45"
+  },
+  {
+    "email": "24bcs171@ietdavv.edu.in",
+    "uniqueCode": "46"
+  },
+  {
+    "email": "prathmeshkumarsingh46395@gmail.com",
+    "uniqueCode": "47"
+  },
+  {
+    "email": "24bcs152@ietdavv.edu.in",
+    "uniqueCode": "48"
+  },
+  {
+    "email": "Vinitaporwal40@gmail.com",
+    "uniqueCode": "49"
+  },
+  {
+    "email": "abhisheksharma67658@gmail.com",
+    "uniqueCode": "50"
+  },
+  {
+    "email": "ananyatiwari3092007@gmail.com",
+    "uniqueCode": "51"
+  },
+  {
+    "email": "asthaporwal19@gmail.com",
+    "uniqueCode": "52"
+  },
+  {
+    "email": "rrishiyadav1919@gmail.com",
+    "uniqueCode": "53"
+  },
+  {
+    "email": "surajbhawsar014@gmail.com",
+    "uniqueCode": "54"
+  },
+  {
+    "email": "princynanno@gmail.com",
+    "uniqueCode": "55"
+  },
+  {
+    "email": "antima.singh.csrl@gmail.com",
+    "uniqueCode": "56"
+  },
+  {
+    "email": "khushibarkur@gmail.com",
+    "uniqueCode": "57"
+  },
+  {
+    "email": "ahirwarsonam063@gmail.com",
+    "uniqueCode": "58"
+  },
+  {
+    "email": "aryanmeena93023@gmail.com",
+    "uniqueCode": "59"
+  },
+  {
+    "email": "aisha.21217940@gmail.com",
+    "uniqueCode": "60"
+  },
+  {
+    "email": "luckykaran56@gmail.com",
+    "uniqueCode": "61"
+  },
+  {
+    "email": "Shejolesumesh@gmail.com",
+    "uniqueCode": "62"
+  },
+  {
+    "email": "mitanshjain143@gmail.com",
+    "uniqueCode": "63"
+  },
+  {
+    "email": "piyushsalve2296@gmail.com",
+    "uniqueCode": "64"
+  },
+  {
+    "email": "anahitamodi37@gmail.com",
+    "uniqueCode": "65"
+  },
+  {
+    "email": "khushikhusi587@gmail.com",
+    "uniqueCode": "66"
+  },
+  {
+    "email": "krishnpalsingh7926@gmail.com",
+    "uniqueCode": "67"
+  },
+  {
+    "email": "ysen9224@gmail.com",
+    "uniqueCode": "68"
+  },
+  {
+    "email": "shivanidhanore086@gmail.com",
+    "uniqueCode": "69"
+  },
+  {
+    "email": "np9827016@gmail.com",
+    "uniqueCode": "70"
+  },
+  {
+    "email": "shivanshukhode043@gmail.com",
+    "uniqueCode": "71"
+  },
+  {
+    "email": "officialaachman03@gmail.com",
+    "uniqueCode": "72"
+  },
+  {
+    "email": "24bei051@ietdavv.edu.in",
+    "uniqueCode": "73"
+  },
+  {
+    "email": "stutisahu410@gmail.com",
+    "uniqueCode": "74"
+  },
+  {
+    "email": "kanakk2412@gmail.com",
+    "uniqueCode": "75"
+  },
+  {
+    "email": "Ankushnema20@gmail.com",
+    "uniqueCode": "76"
+  },
+  {
+    "email": "revanimgaonkar06@gmail.com",
+    "uniqueCode": "77"
+  },
+  {
+    "email": "parauhaanjali28@gmail.com",
+    "uniqueCode": "78"
+  },
+  {
+    "email": "bhavyaagrawat7059@gmail.com",
+    "uniqueCode": "79"
+  },
+  {
+    "email": "niteshmali0223@gmail.com",
+    "uniqueCode": "80"
+  },
+  {
+    "email": "aryanneekhra9806@gmail.com",
+    "uniqueCode": "81"
+  },
+  {
+    "email": "patidarpreetam13@gmail.com",
+    "uniqueCode": "82"
+  },
+  {
+    "email": "anaygattani@gmail.com",
+    "uniqueCode": "83"
+  },
+  {
+    "email": "prakratcs@gmail.com",
+    "uniqueCode": "84"
+  },
+  {
+    "email": "adityajnv10@gmail.com",
+    "uniqueCode": "85"
+  },
+  {
+    "email": "gouravyadav0718@gmail.com",
+    "uniqueCode": "86"
+  },
+  {
+    "email": "23btc036@ietdavv.edu.in",
+    "uniqueCode": "87"
+  },
+  {
+    "email": "24bcs031@ietdavv.edu.in",
+    "uniqueCode": "88"
+  },
+  {
+    "email": "nikitamahajan1622002@gmail.com",
+    "uniqueCode": "89"
+  },
+  {
+    "email": "abhaysinghb36@gmail.com",
+    "uniqueCode": "90"
+  },
+  {
+    "email": "24bit025@ietdavv.edu.in",
+    "uniqueCode": "91"
+  },
+  {
+    "email": "jobinfo.twinkal@gmail.com",
+    "uniqueCode": "92"
+  },
+  {
+    "email": "singh.ranvir.0208@gmail.com",
+    "uniqueCode": "93"
+  },
+  {
+    "email": "ishajrathor1616@gmail.com",
+    "uniqueCode": "94"
+  },
+  {
+    "email": "24bcs066@ietdavv.edu.in",
+    "uniqueCode": "95"
+  },
+  {
+    "email": "harshitpatidar011@gmail.com",
+    "uniqueCode": "96"
+  },
+  {
+    "email": "chetandeshmukh2505@gmail.com",
+    "uniqueCode": "97"
+  },
+  {
+    "email": "khamkarmanashvi@gmail.com",
+    "uniqueCode": "98"
+  },
+  {
+    "email": "dandharearpit56@gmail.com",
+    "uniqueCode": "99"
+  },
+  {
+    "email": "Soumyachandel1@gmail.com",
+    "uniqueCode": "100"
+  },
+  {
+    "email": "adityasitola03@gmail.com",
+    "uniqueCode": "101"
+  },
+  {
+    "email": "dhruvij33@gmail.com",
+    "uniqueCode": "102"
+  },
+  {
+    "email": "kartavyakumarkurmi6@gmail.com",
+    "uniqueCode": "103"
+  },
+  {
+    "email": "smrititigga8@gmail.com",
+    "uniqueCode": "104"
+  },
+  {
+    "email": "siyakgupta2308@gmail.com",
+    "uniqueCode": "105"
+  },
+  {
+    "email": "vinitpatidar626@gmail.com",
+    "uniqueCode": "106"
+  },
+  {
+    "email": "satyamjatav56@gmail.com",
+    "uniqueCode": "107"
+  },
+  {
+    "email": "sanjanamandrai12@gmail.com",
+    "uniqueCode": "108"
+  },
+  {
+    "email": "twinkleassudani@gmail.com",
+    "uniqueCode": "109"
+  },
+  {
+    "email": "jpalak233@gmail.com",
+    "uniqueCode": "110"
+  },
+  {
+    "email": "kewatevansh01@gmail.com",
+    "uniqueCode": "111"
+  },
+  {
+    "email": "ms1293369@gamil.com",
+    "uniqueCode": "112"
+  },
+  {
+    "email": "lakshyadeepkale@gmail.com",
+    "uniqueCode": "113"
+  },
+  {
+    "email": "jayeshbetav16@gmail.com",
+    "uniqueCode": "114"
+  },
+  {
+    "email": "1anadi1sharma@gmail.com",
+    "uniqueCode": "115"
+  },
+  {
+    "email": "Jainchiku408@gmail.com",
+    "uniqueCode": "116"
+  },
+  {
+    "email": "ankushsingh8871@gmail.com",
+    "uniqueCode": "117"
+  },
+  {
+    "email": "bhagyaasati@gmail.com",
+    "uniqueCode": "118"
+  },
+  {
+    "email": "dmahi465@gmail.com",
+    "uniqueCode": "119"
+  },
+  {
+    "email": "sonimahak21@gmail.com",
+    "uniqueCode": "120"
+  },
+  {
+    "email": "gmukati03@gmail.com",
+    "uniqueCode": "121"
+  },
+  {
+    "email": "moksh.jain.dhar@gmail.com",
+    "uniqueCode": "122"
+  },
+  {
+    "email": "amishmahajan26@gmail.com",
+    "uniqueCode": "123"
+  },
+  {
+    "email": "gehnabhawsar1@gmail.com",
+    "uniqueCode": "124"
+  },
+  {
+    "email": "aadeshj646@gmail.com",
+    "uniqueCode": "125"
+  },
+  {
+    "email": "mirdeeba7@gmail.com",
+    "uniqueCode": "126"
+  },
+  {
+    "email": "5132tanish@gmail.com",
+    "uniqueCode": "127"
+  },
+  {
+    "email": "yashtomar14012007@gmail.com",
+    "uniqueCode": "128"
+  },
+  {
+    "email": "anujtibdewal7@gmail.com",
+    "uniqueCode": "129"
+  },
+  {
+    "email": "apksharathore6@gmail.com",
+    "uniqueCode": "130"
+  },
+  {
+    "email": "ishanshahi200@gmail.com",
+    "uniqueCode": "131"
+  },
+  {
+    "email": "arpitkumarguptaakg@gmail.com",
+    "uniqueCode": "132"
+  },
+  {
+    "email": "jodhwanijayant1313@gmail.com",
+    "uniqueCode": "133"
+  },
+  {
+    "email": "anshikatrivedi098@gmail.com",
+    "uniqueCode": "134"
+  },
+  {
+    "email": "23bcs070@ietdavv.edu.in",
+    "uniqueCode": "135"
+  },
+  {
+    "email": "shubhisaraf.13@gmail.com",
+    "uniqueCode": "136"
+  },
+  {
+    "email": "aliayaan28506@gmail.com",
+    "uniqueCode": "137"
+  },
+  {
+    "email": "palakdubey0306@gmail.com",
+    "uniqueCode": "138"
+  },
+  {
+    "email": "vikaskelwa1497@gmail.com",
+    "uniqueCode": "139"
+  },
+  {
+    "email": "agaurhar@gmail.com",
+    "uniqueCode": "140"
+  },
+  {
+    "email": "kanhaagrawal272@gmail.com",
+    "uniqueCode": "141"
+  },
+  {
+    "email": "Sneharaghuwanshi05@gmail.com",
+    "uniqueCode": "142"
+  },
+  {
+    "email": "a2043728022@gmail.com",
+    "uniqueCode": "143"
+  },
+  {
+    "email": "aagamjain762@gmail.com",
+    "uniqueCode": "144"
+  },
+  {
+    "email": "24bit120@ietdavv.edu.in",
+    "uniqueCode": "145"
+  },
+  {
+    "email": "ishakumbhkar958@gmail.com",
+    "uniqueCode": "146"
+  },
+  {
+    "email": "adityakumawat709@gmail.com",
+    "uniqueCode": "147"
+  },
+  {
+    "email": "ami.tated04@gmail.com",
+    "uniqueCode": "148"
+  },
+  {
+    "email": "sumit7067singh@gmail.com",
+    "uniqueCode": "149"
+  },
+  {
+    "email": "raotanishq082@gmail.com",
+    "uniqueCode": "150"
+  },
+  {
+    "email": "nainachourasia21@gmail.com",
+    "uniqueCode": "151"
+  },
+  {
+    "email": "sheikhrafe786@gmail.com",
+    "uniqueCode": "152"
+  },
+  {
+    "email": "24bcv045@IET-DAVV.edu.in",
+    "uniqueCode": "153"
+  },
+  {
+    "email": "ap5934400@gmail.com",
+    "uniqueCode": "154"
+  },
+  {
+    "email": "jayeshshastri0993@gmail.com",
+    "uniqueCode": "155"
+  },
+  {
+    "email": "priyanshuarya994@gmail.com",
+    "uniqueCode": "156"
+  },
+  {
+    "email": "raghuwanshinainsi@gmail.com",
+    "uniqueCode": "157"
+  },
+  {
+    "email": "reenarewer@gmail.com",
+    "uniqueCode": "158"
+  },
+  {
+    "email": "hetvirathore248@gmail.com",
+    "uniqueCode": "159"
+  },
+  {
+    "email": "shridharpandey500000@gmail.com",
+    "uniqueCode": "160"
+  },
+  {
+    "email": "kalawatiahirwar16@gmail.com",
+    "uniqueCode": "161"
+  },
+  {
+    "email": "kabiryadav588@gmail.com",
+    "uniqueCode": "162"
+  },
+  {
+    "email": "patidarbhawana2006@gmail.com",
+    "uniqueCode": "163"
+  },
+  {
+    "email": "muskan66504@gmail.com",
+    "uniqueCode": "164"
+  },
+  {
+    "email": "agrawalyash2206@gmail.com",
+    "uniqueCode": "165"
+  },
+  {
+    "email": "nihalparteti26@gmail.com",
+    "uniqueCode": "166"
+  },
+  {
+    "email": "palakmaheshwari999@gmail.com",
+    "uniqueCode": "167"
+  },
+  {
+    "email": "raghav21184@gmail.com",
+    "uniqueCode": "168"
+  },
+  {
+    "email": "sidranoorkhan11@gmail.com",
+    "uniqueCode": "169"
+  },
+  {
+    "email": "24bcs029@ietdavv.edu.in",
+    "uniqueCode": "170"
+  },
+  {
+    "email": "Tisyadhruv@gmail.com",
+    "uniqueCode": "171"
+  },
+  {
+    "email": "121yash212@gmail.com",
+    "uniqueCode": "172"
+  },
+  {
+    "email": "shubhikashukla24@gmail.com",
+    "uniqueCode": "173"
+  },
+  {
+    "email": "shivrashi282@gmail.com",
+    "uniqueCode": "174"
+  },
+  {
+    "email": "divyanshivaishnav554@gmail.com",
+    "uniqueCode": "175"
+  },
+  {
+    "email": "24bcs143@ietdavv.edu.in",
+    "uniqueCode": "176"
+  },
+  {
+    "email": "24bit080@ietdavv.edu.in",
+    "uniqueCode": "177"
+  },
+  {
+    "email": "beingkhushi22@gmail.com",
+    "uniqueCode": "178"
+  },
+  {
+    "email": "parasmanipatel2010@gmail.com",
+    "uniqueCode": "179"
+  },
+  {
+    "email": "24bit152@ietdavv.edu.in",
+    "uniqueCode": "180"
+  },
+  {
+    "email": "charvijain77@gmail.com",
+    "uniqueCode": "181"
+  },
+  {
+    "email": "divyanshkarma98@gmail.com",
+    "uniqueCode": "182"
+  },
+  {
+    "email": "vrandamundra27@gmail.com",
+    "uniqueCode": "183"
+  },
+  {
+    "email": "ashwini.gupta.9@gmail.com",
+    "uniqueCode": "184"
+  },
+  {
+    "email": "tusharrthr007@gmail.com",
+    "uniqueCode": "185"
+  },
+  {
+    "email": "24bcs064@ietdavv.edu.in",
+    "uniqueCode": "186"
+  },
+  {
+    "email": "zaki.khan.8311@gmail.com",
+    "uniqueCode": "187"
+  },
+  {
+    "email": "ritikvora1911@gmail.com",
+    "uniqueCode": "188"
+  },
+  {
+    "email": "ayushi.parmar2610@gmail.com",
+    "uniqueCode": "189"
+  },
+  {
+    "email": "shiv07raghu@gmail.com",
+    "uniqueCode": "190"
+  },
+  {
+    "email": "ambersaluja2006@gmail.com",
+    "uniqueCode": "191"
+  },
+  {
+    "email": "naitikboriya16@gmail.com",
+    "uniqueCode": "192"
+  },
+  {
+    "email": "24bcs032@ietdavv.edu.in",
+    "uniqueCode": "193"
+  },
+  {
+    "email": "24bcs133@ietdavv.edu.in",
+    "uniqueCode": "194"
+  },
+  {
+    "email": "ranjeetsitole2007@gmail.com",
+    "uniqueCode": "195"
+  },
+  {
+    "email": "mouryarahul068@gmail.com",
+    "uniqueCode": "196"
+  },
+  {
+    "email": "anshikatiwari9990@gmail.com",
+    "uniqueCode": "197"
+  },
+  {
+    "email": "24btc124@ietdavv.edu.in",
+    "uniqueCode": "198"
+  },
+  {
+    "email": "adityasitola03@gmail.com",
+    "uniqueCode": "199"
+  },
+  {
+    "email": "Sarthakpatidar27sp@gmail.com",
+    "uniqueCode": "200"
+  },
+  {
+    "email": "raoanuhp7@gmail.com",
+    "uniqueCode": "201"
+  },
+  {
+    "email": "latathakur1790@gmail.com",
+    "uniqueCode": "202"
+  },
+  {
+    "email": "deepanshuvanchhode@gmail.com",
+    "uniqueCode": "203"
+  },
+  {
+    "email": "naveenpatel21062006@gmail.com",
+    "uniqueCode": "204"
+  },
+  {
+    "email": "24btc137@ietdavv.edu.in",
+    "uniqueCode": "205"
+  },
+  {
+    "email": "sarthaksomtiya@gmail.com",
+    "uniqueCode": "206"
+  },
+  {
+    "email": "harshitsiddharth3@gmail.com",
+    "uniqueCode": "207"
+  },
+  {
+    "email": "vedikawadgaonkar@gmail.com",
+    "uniqueCode": "208"
+  },
+  {
+    "email": "pandeypranjali658@gmail.com",
+    "uniqueCode": "209"
+  },
+  {
+    "email": "lakshyarohitas127@gmail.com",
+    "uniqueCode": "210"
+  },
+  {
+    "email": "omniranjan108@gmail.com",
+    "uniqueCode": "211"
+  },
+  {
+    "email": "vishnukhatik2004@gmail.com",
+    "uniqueCode": "212"
+  },
+  {
+    "email": "jyotiarya2006@gmail.com",
+    "uniqueCode": "213"
+  },
+  {
+    "email": "mohitmotwani002@gmail.com",
+    "uniqueCode": "214"
+  },
+  {
+    "email": "rishi.73.patel@gmail.com",
+    "uniqueCode": "215"
+  },
+  {
+    "email": "shivanshu200720072007@gmail.com",
+    "uniqueCode": "216"
+  },
+  {
+    "email": "ramr887685@gmail.com",
+    "uniqueCode": "217"
+  },
+  {
+    "email": "dsbaghel2231@gmail.com",
+    "uniqueCode": "218"
+  },
+  {
+    "email": "rajputsameeksha2005@gmail.com",
+    "uniqueCode": "219"
+  },
+  {
+    "email": "vishalkirar091@gmail.com",
+    "uniqueCode": "220"
+  },
+  {
+    "email": "sejalsomkuwar1@gmail.com",
+    "uniqueCode": "221"
+  },
+  {
+    "email": "24btc011@ietdavv.edu.in",
+    "uniqueCode": "222"
+  },
+  {
+    "email": "24btc013@ietdavv.edu.in",
+    "uniqueCode": "223"
+  },
+  {
+    "email": "24bcb047@ietdavv.edu.in",
+    "uniqueCode": "224"
+  },
+  {
+    "email": "arnav.kekre.2807@gmail.com",
+    "uniqueCode": "225"
+  },
+  {
+    "email": "24btc118@ietdavv.edu.in",
+    "uniqueCode": "226"
+  },
+  {
+    "email": "aayushchourasia2022@gmail.com",
+    "uniqueCode": "227"
+  }
+]
